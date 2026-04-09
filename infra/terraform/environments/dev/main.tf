38775b9bdb1b0beddf2c5dc0aaf1525cd2dac593
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-04-09 16:24:44 +0900

    infra: dev 환경에 IAM/EC2 모듈 호출 추가

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -26,3 +26,19 @@ module "security_group" {
   vpc_id  = module.vpc.vpc_id
   my_ip   = var.my_ip
 }
+
+module "iam" {
+  source  = "../../modules/iam"
+  project = var.project
+  env     = var.env
+}
+
+module "ec2" {
+  source                = "../../modules/ec2"
+  project               = var.project
+  env                   = var.env
+  subnet_id             = module.vpc.public_subnet_id
+  security_group_id     = module.security_group.k3s_sg_id
+  instance_profile_name = module.iam.instance_profile_name
+  key_name              = var.key_name
+}
--- infra/terraform/environments/dev/variables.tf
+++ infra/terraform/environments/dev/variables.tf
@@ -1,19 +1,24 @@
 variable "aws_region" {
-  type        = string
-  default     = "ap-northeast-2"
+  type    = string
+  default = "ap-northeast-2"
 }
 
 variable "project" {
-  type        = string
-  default     = "disasterkok"
+  type    = string
+  default = "disasterkok"
 }
 
 variable "env" {
-  type        = string
-  default     = "dev"
+  type    = string
+  default = "dev"
 }
 
 variable "my_ip" {
   type        = string
   description = "Your IP for SSH access"
+}
+
+variable "key_name" {
+  type        = string
+  description = "EC2 Key Pair 이름"
 }
